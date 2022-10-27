f7e6ef44d6d47c16f107c8632310491627fd5b70
Rahul Chaurasia <rahulrchaurasia@gmail.com>
2022-10-27 17:43:11 +0530

    Add downloadFile

--- app/src/main/java/com/example/jetpackdemo/Utility/Utility.kt
+++ app/src/main/java/com/example/jetpackdemo/Utility/Utility.kt
@@ -1,5 +1,6 @@
 package com.example.jetpackdemo.Utility
 
+import android.content.ContentValues
 import android.content.Context
 import android.content.Intent
 import android.graphics.Bitmap
@@ -7,15 +8,19 @@ import android.graphics.BitmapFactory
 import android.net.Uri
 import android.os.Build
 import android.os.Environment
+import android.provider.MediaStore
 import android.provider.Settings
 import android.util.Base64
 import android.util.Base64.encodeToString
 import android.util.Log
 import androidx.core.content.FileProvider
+import androidx.core.net.toUri
 import com.example.jetpackdemo.BuildConfig
 import java.io.ByteArrayOutputStream
 import java.io.File
+import java.io.FileOutputStream
 import java.io.IOException
+import java.net.URL
 import java.text.SimpleDateFormat
 import java.util.*
 
@@ -128,6 +133,7 @@ object Utility {
 //    }
 
 
+
     fun loadWebViewUrlInBrowser(context: Context, url: String?) {
         Log.d("URL", url!!)
         val browserIntent = Intent(Intent.ACTION_VIEW)
@@ -136,5 +142,49 @@ object Utility {
         }
         context.startActivity(browserIntent)
     }
+
+    /****************************************************************
+    //Note : Download any Type of File and Images Using URL
+    ****************************************************************/
+     fun downloadFileFromUri( context: Context, url: String,mimeType: String, filename: String?): Uri? {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+
+            val contentValues = ContentValues().apply {
+                put(MediaStore.MediaColumns.DISPLAY_NAME, filename)
+                put(MediaStore.MediaColumns.MIME_TYPE, mimeType)
+                put(MediaStore.MediaColumns.RELATIVE_PATH, Environment.DIRECTORY_DOWNLOADS)
+            }
+
+            val resolver = context.applicationContext.contentResolver
+            val uri = resolver.insert(MediaStore.Downloads.EXTERNAL_CONTENT_URI, contentValues)
+            return if (uri != null) {
+                URL(url).openStream().use { input ->
+                    resolver.openOutputStream(uri).use { output ->
+                        input.copyTo(output!!, DEFAULT_BUFFER_SIZE)
+                    }
+
+                }
+                uri
+            } else {
+                null
+            }
+
+        } else {
+
+            val target = File(
+                Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS),
+                filename
+            )
+            URL(url).openStream().use { input ->
+                FileOutputStream(target).use { output ->
+                    input.copyTo(output)
+                }
+            }
+
+            return target.toUri()
+        }
+    }
+
+
 
 }
